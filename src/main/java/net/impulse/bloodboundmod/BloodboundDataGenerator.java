abbd2a8eee9635a2c0bdce8f17633d8792e6e435
Leandro11306908 <251964@student.scalda.nl>
2026-09-17 10:59:36 +0200

    Initial commit

---
+++ src/main/java/net/impulse/bloodboundmod/BloodboundDataGenerator.java
@@ -0,0 +1,11 @@
+package net.impulse.bloodboundmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BloodboundDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
